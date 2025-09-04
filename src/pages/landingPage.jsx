5cbe69e8f6c498768884010fe4a0fe1fb0b87ab5
Garv Sharma <garvsharma200512@gmail.com>
2025-09-04 13:10:13 +0530

    trash code

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -123,31 +123,7 @@ const careOptions = [
 ];
 
 
-const Rating = ({ score }) => {
-  
-  return (
-    <div className="flex items-center text-yellow-400">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4 mr-1 fill-current"
-        viewBox="0 0 20 20"
-      >
-        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.96a1 1 0 00.95.69h4.161c.969 0 1.371 1.24.588 1.81l-3.374 2.454a1 1 0 00-.364 1.118l1.287 3.961c.3.921-.755 1.688-1.54 1.118l-3.374-2.454a1 1 0 00-1.176 0l-3.374 2.454c-.785.57-1.84-.197-1.54-1.118l1.287-3.96a1 1 0 00-.364-1.119L2.83 9.395c-.783-.57-.38-1.81.588-1.81h4.16a1 1 0 00.951-.69l1.286-3.96z" />
-      </svg>
-      <span className="text-gray-700 font-semibold text-sm">{score}</span>
-    </div>
-  );
-};
 
-// Helper component for the bullet points
-const BulletPoint = ({ text }) => {
-  return (
-    <li className="flex items-start space-x-2">
-      <div className="w-1.5 h-1.5 mt-2 bg-blue-500 rounded-full flex-shrink-0"></div>
-      <span className="text-gray-600">{text}</span>
-    </li>
-  );
-};
 
 export default function LandingPage({user}) {
   const [selectedSymptom, setSelectedSymptom] = useState(null);
