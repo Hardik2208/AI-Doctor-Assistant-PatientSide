0f91756d47dc104429d587ff6e5d02b06e23aabe
atharvgarg18 <24bcb013@gmail.com>
2025-09-27 22:40:43 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+//test#101
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
